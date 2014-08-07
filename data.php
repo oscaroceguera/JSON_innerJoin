8bb7df3e6671fccc3cab1dfb3f5761cbc77df8c3
Sergio Daniel Xalambrí <sergio@xalambri.com.ar>
2014-08-06 23:16:57 -0300

    arreglado el problema
    El problema se debía a la codificación de los datos en la base de datos,
    estaba en latin1, fijate que este siempre en utf8, ahora hice que
    funcione con latin1, en el código esta explicado con comentarios.

--- data.php
+++ data.php
@@ -1,16 +1,35 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "root";
-$dbName = "prueba";
+$host     = "localhost";
+$user     = "root";
+$password = "";
+$dbName   = "innerjoin";
 
 $conexion = mysqli_connect($host,$user,$password,$dbName);
 
-$consulta = "SELECT A.pregunta,A.recomendacion,B.nomModulo FROM CAT_PREGUNTAS AS A INNER JOIN modulos_diagnosticos AS B ON A.id_modulo=B.id_modulo";
+// en la consulta saque lo de AS A y AS B, como pregunta, recomendacion y
+// nomModulo no se repiten entre las tablas no lo necesitas y podes referirte
+// a cada tabla simplemente con su nombre, igual si se lo agregás anda.
+$consulta = "SELECT pregunta, recomendacion, nomModulo FROM cat_preguntas INNER JOIN modulos_diagnosticos ON cat_preguntas.id_modulo = modulos_diagnosticos.id_modulo";
 
+// ejecuto la consulta
 $query = mysqli_query($conexion,$consulta);
 
-$json = json_encode($query);
+$arr = array();
+while($row = mysqli_fetch_object($query)){
+  // en cada vuelta del ciclo hago un array_push
+  // en este array_push agrego al array $arr un nuevo array con dos posiciones,
+  // pregunta y modulo.
+  // y este array lo encodeo en utf8 usando array_map ya que está mal
+  // encodeado en la base de datos
+  array_push($arr, array_map('utf8_encode', array(
+      'pregunta' => $row->pregunta,
+      'modulo'   => $row->nomModulo
+    )));
+}
 
+// lo convierto a JSON
+$json = json_encode($arr);
+
+// devuelvo el JSON
 echo $json;
