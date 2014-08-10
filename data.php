fc0df4bd879e91699b074b8bd9386dcd04b489b0
Oscar E. Oceguera Bibriesca <oscaroceguera@gmail.com>
2014-08-10 14:07:41 -0600

    mayusculas

--- data.php
+++ data.php
@@ -2,15 +2,15 @@
 
 $host     = "localhost";
 $user     = "root";
-$password = "";
-$dbName   = "innerjoin";
+$password = "root";
+$dbName   = "prueba";
 
 $conexion = mysqli_connect($host,$user,$password,$dbName);
 
 // en la consulta saque lo de AS A y AS B, como pregunta, recomendacion y
 // nomModulo no se repiten entre las tablas no lo necesitas y podes referirte
 // a cada tabla simplemente con su nombre, igual si se lo agregás anda.
-$consulta = "SELECT pregunta, recomendacion, nomModulo FROM cat_preguntas INNER JOIN modulos_diagnosticos ON cat_preguntas.id_modulo = modulos_diagnosticos.id_modulo";
+$consulta = "SELECT pregunta, recomendacion, nomModulo FROM CAT_PREGUNTAS INNER JOIN modulos_diagnosticos ON cat_preguntas.id_modulo = modulos_diagnosticos.id_modulo";
 
 // ejecuto la consulta
 $query = mysqli_query($conexion,$consulta);
